cfc9de8f9759dbe44547f8a3ad7416bd6c4eca30
GarrettRector <garrett@grector.dev>
2025-03-09 10:10:51 -0400

    Auton PID Tuning

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -61,7 +61,7 @@ public class Swerve extends SubsystemBase {
             (speeds, feedforwards) -> driveRobotRelative(speeds), // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds. Also optionally outputs individual module feedforwards
             new PPHolonomicDriveController( // PPHolonomicController is the built in path following controller for holonomic drive trains
                     new PIDConstants(5, 0, 0), // Translation PID constants
-                    new PIDConstants(4, 0, 0) // Rotation PID constants
+                    new PIDConstants(2, 0, 0) // Rotation PID constants
             ),
             config, // The robot configuration
             () -> {
